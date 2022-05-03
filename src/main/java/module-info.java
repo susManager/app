0e927bdef92672d65521c7fdf3ffa0f29f12c7c7
Optinux <tobias.eglseder2@gmail.com>
2022-05-03 19:09:40 +0200

    allow packaging

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module susmanager {
   requires javafx.controls;
   requires javafx.fxml;
   requires javafx.media;
+  requires javafx.graphics;
 
   opens susmanager to javafx.fxml;
   exports susmanager ;
---
+++ src/main/java/susmanager/Entry.java
@@ -0,0 +1,8 @@
+package susmanager;
+
+public class Entry {
+
+  public static void main(String[] args) {
+    App.main(args);
+  }
+}
