b0741d46578a625be676930fe0bfa94fe437693b
Tobias Eglseder <eglsedert@schule.local>
2022-05-25 09:22:28 +0200

    prepare for color themes

--- src/main/java/susmanager/Settings.java
+++ src/main/java/susmanager/Settings.java
@@ -3,11 +3,19 @@ package susmanager;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
 
 public class Settings {
 
   @FXML
   private ImageView trollFace;
+
+  @FXML
+  private Pane settings_background;
+
+  @FXML
+  private Pane title_banner;
 
   @FXML
   private void playThudSound() {
@@ -43,4 +51,19 @@ public class Settings {
   private void switchToLogin() throws IOException {
     App.setRoot("login");
   }
+
+  @FXML
+  private void color_switchToDefault() {
+     System.out.println("switched to default color theme!");
+    settings_background.setStyle("-fx-background-color: #050714");
+    title_banner.setStyle("-fx-background-color: #1f1543");
+  }
+
+  @FXML
+  private void color_switchToNord() {
+    System.out.println("switched to Nord color theme!");
+    settings_background.setStyle("-fx-background-color: #617482");
+    title_banner.setStyle("-fx-background-color: #88c0d0");
+  }
+
 }
