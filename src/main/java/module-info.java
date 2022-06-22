56228ef016f50318753ee583675df1b80c6b1c3f
Optinux <tobias.eglseder2@gmail.com>
2022-06-23 00:02:28 +0200

    include fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module susmanager {
   requires javafx.controls;
+  requires javafx.base;
   requires javafx.fxml;
   requires javafx.media;
   requires javafx.graphics;
   requires susManager.lib;
 
   opens susmanager to javafx.fxml;
-  exports susmanager ;
+  exports susmanager to javafx.graphics;
 }
